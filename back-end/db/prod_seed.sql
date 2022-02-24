c13c0c4acb73da2c017d95b514738546254c313e
Jerry De Jesus <jdejesus7@live.com>
2022-02-24 10:48:58 -0500

    edited prod_schema and prod_seed files

--- back-end/db/prod_seed.sql
+++ back-end/db/prod_seed.sql
@@ -1,8 +1,8 @@
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+INSERT INTO shoes (name, description, footwear_type, price, rating, featured, image) VALUES
+('Air Jordan 1', 'All Time Classic', 'Sneakers', 210, 5, false, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDzWQN2t6G_-L6oMslH-w0cRaFEKbFBC0hD8DKykaZwUa39xnsB1FIz90KDcBALefUYPo&usqp=CAU'),
+('Crocs', 'Errand-Runners', 'Clogs', 100, 3, false, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI2JnKcEsZKZsRMWccpTsUr0bX2Diw65EwDgbIJt8M06qF_yNNT5wvJOQQnQahmfLxP9Y&usqp=CAU'),
+('Converse Chuck Taylors', 'Sports? In these?','Sneakers', 70, 4, false, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1_4Z7WhTm7JxeSAttGuCAu6Yb_AD4q9ElfQ&usqp=CAU'),
+('Timberland', 'Yerrrr', 'Work Boots', 170, 4, true, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSytQzLByEJHhFUgXC8ePrzVXmY4PonjTLRMA&usqp=CAU'),
+('Nike SB Dunk Low', 'Ben and Jerrys Edition', 'Sneakers', 1900, 3, true, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB_0uSlZ9FyQfq1ydKdLJMyEozhHMHMamr6KVgRssJMUsVflkh31bf-uMP0reIrQMj78Y&usqp=CAU'),
+('Adidas Yeezy Boost 350', 'Brain child of Kanye West AKA Yezos', 'Sneakers', 220, 4, false, 'https://media.vogue.fr/photos/5fb690eeb71579473cb07e47/master/w_1600,c_limit/%20Adidas%20Yeezy%20Boost%20350%20V2%20%E2%80%9CCarbon%E2%80%9D.jpg'),
+('Nike Space Hippie 03', 'Funny looking', 'Sneakers', 330, 2, false, 'https://media.vogue.fr/photos/5fb6911ae8560ed945b767b6/master/w_1600,c_limit/Nike%20Space%20Hippie%2003%20Vast%20Grey%20Hyper%20Crimson.jpg');
